27b7738bbe1e1aea35a0e856fbe82a212fd5dcab
Valentina <36822237+vlesovaya@users.noreply.github.com>
2021-11-03 15:52:55 +0100

    Added like for cards (#2)

--- page/index.js
+++ page/index.js
@@ -10,4 +10,18 @@ profileEditButton.addEventListener('click', function (evt) {
 popupCloseButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   renderPopup.classList.remove('popup_opened');
-})
+});
+
+let likeButtons = document.querySelectorAll('.gallery__like');
+
+for (let i = 0; i < likeButtons.length; i += 1) {
+  let likeButton = likeButtons[i];
+  likeButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    if (likeButton.classList.contains('gallery__like_active')) {
+      likeButton.classList.remove('gallery__like_active');
+    } else {
+      likeButton.classList.add('gallery__like_active');
+    }
+  });
+}
