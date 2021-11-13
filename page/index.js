8b2e7c200ddda07e92e8d1ef1e45355145319ce3
valentinalesovaa <valentina-lesovaya@yandex.ru>
2021-11-13 13:33:10 +0100

    Added cards from array

--- page/index.js
+++ page/index.js
@@ -104,6 +104,7 @@ function addCard(name, link) {
   const cardElement = cardTemplate.querySelector('.gallery__item').cloneNode(true);
 
   cardElement.querySelector('.gallery__photo').src = link;
+  cardElement.querySelector('.gallery__photo').alt = name;
   cardElement.querySelector('.gallery__title').textContent = name;
 
   cardElement.querySelector('.gallery__like').addEventListener('click', function (evt) {
@@ -138,3 +139,36 @@ function createFormSubmitHandler(evt) {
   closeCreatePopup(evt);
 }
 createFormElement.addEventListener('submit', createFormSubmitHandler);
+
+// add cards from array
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function (item) {
+  addCard(item.name, item.link);
+});
